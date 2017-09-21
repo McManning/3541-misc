d9c6135d271376a536bb52e31dd8ef0b448b75eb
Chase McManning <mcmanning.1@osu.edu>
2017-09-20 22:04:27 -0400

    Add algorithm to build a spanning tree of doors between regions

--- Assets/Scripts/Dungeon.cs
+++ Assets/Scripts/Dungeon.cs
@@ -11,11 +11,13 @@ public class Dungeon : MonoBehaviour
         public IntVector2 Position { get; internal set; }
         public bool IsWall { get; internal set; }
         public int Region { get; internal set; }
+        public bool IsConnector { get; internal set; }
 
         public CellMetadata(IntVector2 position)
         {
             Position = position;
             IsWall = true;
+            IsConnector = false;
             Region = 0;
         }
     }
@@ -39,12 +41,24 @@ public class Dungeon : MonoBehaviour
     public DungeonCell wallPrefab;
 
     /// <summary>
+    /// Prefab GameObject used to render doors between regions
+    /// </summary>
+    public DungeonCell doorPrefab;
+
+    /// <summary>
     /// Amount of "randomness" for the tree growing algorithm.
     /// A value of 1 will create very windy hallways, while a 
     /// value of 0 will create as straight of halls as possible
     /// </summary>
     [Range(0.0f, 1.0f)]
     public float treeGrowingRandomness;
+
+    /// <summary>
+    /// Chance that extra doors will be added to connect regions
+    /// and make less of a true spanning tree
+    /// </summary>
+    [Range(0.0f, 1.0f)]
+    public float doorLoopChance;
 
     /// <summary>
     /// Maximum rooms to be generated in the dungeon
@@ -86,6 +100,38 @@ public class Dungeon : MonoBehaviour
 	}
 
     /// <summary>
+    /// Run through the full generator process
+    /// </summary>
+    public void Generate()
+    {
+        currentRegion = 0;
+
+        cells = new CellMetadata[size.x, size.z];
+        openCells = new List<CellMetadata>();
+
+        // Initialize everything in the cell metadata array
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int z = 0; z < size.z; z++)
+            {
+                cells[x, z] = new CellMetadata(new IntVector2(x, z));
+            }
+        }
+
+        // Populate the dungeon with open rooms
+        BuildRooms();
+
+        // In open spaces between rooms, generate perfect mazes
+        BuildMazes();
+
+        // Bridge rooms and mazes with doors
+        BuildDoors();
+
+        // Finally, generate prefabs for every cell
+        BuildPrefabs();
+    }
+
+    /// <summary>
     /// Returns whether the given cell position is in acceptable bounds
     /// </summary>
     /// <param name="position"></param>
@@ -105,35 +151,6 @@ public class Dungeon : MonoBehaviour
     private int RandomOdd(int min, int max)
     {
         return 1 + 2 * (Random.Range(min - 1, max - 1) / 2);
-    }
-
-    /// <summary>
-    /// Run through the full generator process
-    /// </summary>
-    public void Generate()
-    {
-        currentRegion = 0;
-
-        cells = new CellMetadata[size.x, size.z];
-        openCells = new List<CellMetadata>();
-
-        // Initialize everything in the cell metadata array
-        for (int x = 0; x < size.x; x++)
-        {
-            for (int z = 0; z < size.z; z++)
-            {
-                cells[x, z] = new CellMetadata(new IntVector2(x, z));
-            }
-        }
-        
-        // Populate the dungeon with open rooms
-        BuildRooms();
-
-        // In open spaces between rooms, generate perfect mazes
-        BuildMazes();
-
-        // Finally, generate prefabs for every cell
-        BuildPrefabs();
     }
 
     /// <summary>
@@ -197,9 +214,51 @@ public class Dungeon : MonoBehaviour
         cell.Region = currentRegion;
     }
 
+    /// <summary>
+    /// Get the CellMetadata at the specific (x, z)
+    /// </summary>
+    /// <param name="position"></param>
+    /// <returns></returns>
     private CellMetadata GetCell(IntVector2 position)
     {
         return cells[position.x, position.z];
+    }
+
+    /// <summary>
+    /// Get all cells adjacent in the cardinal (and intercardinal) directions of the given cell
+    /// </summary>
+    /// <param name="cell"></param>
+    /// <param name="includeIntercardinals"></param>
+    /// <returns></returns>
+    private List<CellMetadata> GetAdjacentCells(CellMetadata cell, bool includeIntercardinals)
+    {
+        List<CellMetadata> adjacent = new List<CellMetadata>();
+        IntVector2 position;
+
+        // Grab cells in all cardinal directions
+        foreach (IntVector2 direction in IntVector2.Cardinals)
+        {
+            position = cell.Position + direction;
+            if (Contains(position))
+            {
+                adjacent.Add(GetCell(position));
+            }
+        }
+
+        // If they ask for intercardinals, grab those too
+        if (includeIntercardinals)
+        {
+            foreach (IntVector2 direction in IntVector2.Intercardinals)
+            {
+                position = cell.Position + direction;
+                if (Contains(position))
+                {
+                    adjacent.Add(GetCell(position));
+                }
+            }
+        }
+
+        return adjacent;
     }
 
     /// <summary>
@@ -227,6 +286,7 @@ public class Dungeon : MonoBehaviour
 
     /// <summary>
     /// Perform a Tree Growing maze algorithm originating from the given (x, z)
+    /// to fill all unoccupied (walled) space s.t. all halls are walled in
     /// </summary>
     /// <param name="start"></param>
     private void TreeGrowingFloodFill(CellMetadata start)
@@ -235,12 +295,6 @@ public class Dungeon : MonoBehaviour
         List<IntVector2> validDirections;
         IntVector2? lastDirection = null;
         CellMetadata cell;
-
-        // quick iteration tracker so my stupidity doesn't cause 
-        // Unity to hang again :^)
-        int iterations = 0;
-
-        Debug.Log("Tree Growing at " + start.Position.x + ", " + start.Position.z);
         
         // Use a new region for this flood fill maze
         currentRegion += 1;
@@ -314,15 +368,15 @@ public class Dungeon : MonoBehaviour
     /// <returns></returns>
     private bool IsIsolatedCell(CellMetadata cell)
     {
-        int x = cell.Position.x;
-        int z = cell.Position.z;
+        foreach (CellMetadata adjacent in GetAdjacentCells(cell, true))
+        {
+            if (!adjacent.IsWall)
+            {
+                return false;
+            }
+        }
 
-        return 
-            cell.IsWall &&
-            cells[x - 1, z].IsWall && cells[x + 1, z].IsWall &&
-            cells[x, z - 1].IsWall && cells[x, z + 1].IsWall &&
-            cells[x - 1, z - 1].IsWall && cells[x + 1, z + 1].IsWall &&
-            cells[x - 1, z + 1].IsWall && cells[x + 1, z - 1].IsWall;
+        return true;
     }
 
     /// <summary>
@@ -339,7 +393,6 @@ public class Dungeon : MonoBehaviour
             {
                 if (IsIsolatedCell(cells[x, z]))
                 {
-                    Debug.Log(x + ", " + z);
                     return cells[x, z];
                 }
             }
@@ -367,6 +420,154 @@ public class Dungeon : MonoBehaviour
     }
 
     /// <summary>
+    /// Get all unique region IDs of adjacent cells, excluding any 
+    /// that don't have a region ID
+    /// </summary>
+    /// <param name="cell"></param>
+    /// <returns></returns>
+    private HashSet<int> GetAdjacentRegions(CellMetadata cell)
+    {
+        HashSet<int> regions = new HashSet<int>();
+
+        foreach (CellMetadata adjacent in GetAdjacentCells(cell, false))
+        {
+            regions.Add(adjacent.Region);
+        }
+
+        // Remove any "no region" results
+        regions.Remove(0);
+        return regions;
+    }
+
+    /// <summary>
+    /// Get all cells that may bridge a gap between the given region 
+    /// and another adjacent region
+    /// </summary>
+    /// <param name="region"></param>
+    private List<CellMetadata> GetRegionConnectorCells(int region)
+    {
+        List<CellMetadata> connectors = new List<CellMetadata>();
+        HashSet<int> regions;
+
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int z = 0; z < size.z; z++)
+            {
+                regions = GetAdjacentRegions(cells[x, z]);
+                if (regions.Count > 1 && regions.Contains(region))
+                {
+                    connectors.Add(cells[x, z]);
+                }
+            }
+        }
+
+        return connectors;
+    }
+
+    /// <summary>
+    /// Return *all* connectors between different regions
+    /// </summary>
+    /// <returns></returns>
+    private List<CellMetadata> GetAllConnectorCells()
+    {
+        List<CellMetadata> connectors = new List<CellMetadata>();
+        HashSet<int> regions;
+
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int z = 0; z < size.z; z++)
+            {
+                regions = GetAdjacentRegions(cells[x, z]);
+                if (regions.Count > 1)
+                {
+                    connectors.Add(cells[x, z]);
+                }
+            }
+        }
+
+        return connectors;
+    }
+
+    private void CarveDoor(CellMetadata cell)
+    {
+        cell.IsConnector = true;
+        cell.IsWall = false;
+    }
+    
+    /// <summary>
+    /// Change all cells of the given regions to the new region
+    /// </summary>
+    /// <param name="regions"></param>
+    /// <param name="newRegion"></param>
+    private void UpdateRegions(HashSet<int> regions, int newRegion)
+    {
+        for (int x = 0; x < size.x; x++)
+        {
+            for (int z = 0; z < size.z; z++)
+            {
+                if (regions.Contains(cells[x, z].Region)) {
+                    cells[x, z].Region = newRegion;
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Go through all regions generated (distinct rooms and maze halls) 
+    /// and connect them together via a simple spanning tree, creating 
+    /// a door cell between each.
+    /// 
+    /// Note that this also has some randomness in it to also allow additional
+    /// loops (since a true spanning tree is boring...)
+    /// </summary>
+    private void BuildDoors()
+    {
+        List<CellMetadata> allConnectors = GetAllConnectorCells();
+        List<CellMetadata> connectors = new List<CellMetadata>();
+        HashSet<int> regions;
+        HashSet<int> mergedRegions = new HashSet<int>() { 1 };
+        CellMetadata door;
+
+        int looper = 0;
+        while (allConnectors.Count > 0 && looper++ < 100)
+        {
+            // Update our list with connectors that connect the merged
+            // regions to unmerged regions
+            connectors.Clear();
+            foreach (CellMetadata cell in allConnectors)
+            {
+                regions = GetAdjacentRegions(cell);
+                regions.IntersectWith(mergedRegions);
+                if (regions.Count > 0)
+                {
+                    connectors.Add(cell);
+                }
+            }
+
+            // Pick a connector at random to turn into a door
+            door = connectors[Random.Range(0, connectors.Count)];
+
+            CarveDoor(door);
+            connectors.Remove(door);
+            allConnectors.Remove(door);
+
+            // Merge all adjacent regions to the door
+            mergedRegions.UnionWith(GetAdjacentRegions(door));
+
+            // For all other connectors that *only* connect to merged regions,
+            // either remove them or give them a (small) chance of becoming
+            // an extra door (to be less of a spanning tree)
+            foreach (CellMetadata cell in connectors)
+            {
+                regions = GetAdjacentRegions(cell);
+                if (regions.IsSubsetOf(mergedRegions)) {
+                    allConnectors.Remove(cell);
+                }
+            }
+        }
+    }
+
+    /// <summary>
     /// Place a prefab from the cell metadata in the given position
     /// </summary>
     /// <param name="position"></param>
@@ -374,7 +575,11 @@ public class Dungeon : MonoBehaviour
     {
         DungeonCell cell;
         
-        if (cells[position.x, position.z].IsWall)
+        if (cells[position.x, position.z].IsConnector)
+        {
+            cell = Instantiate(doorPrefab) as DungeonCell;
+        }
+        else if (cells[position.x, position.z].IsWall)
         {
             cell = Instantiate(wallPrefab) as DungeonCell;
         }
@@ -382,15 +587,15 @@ public class Dungeon : MonoBehaviour
         {
             cell = Instantiate(floorPrefab) as DungeonCell;
         }
-        
+
         cell.LoadMetadata(cells[position.x, position.z]);
-        
+
         // Transform cell to a local space position relative to its (x, z)
         // Ensuring the parent Dungeon is centered in the generated cells
         cell.transform.parent = transform;
         cell.transform.localPosition = new Vector3(
-            position.x - size.x * 0.5f + 0.5f, 
-            0f, 
+            position.x - size.x * 0.5f + 0.5f,
+            0f,
             position.z - size.z * 0.5f + 0.5f
         );
     }
