9edcb49c28b4a80d2576b9663b300ac9df485e1c
Chase McManning <mcmanning.1@osu.edu>
2017-09-23 17:48:39 -0400

    Add basic scene rotation on the press of 0

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,22 +7,44 @@ public class GameManager : MonoBehaviour
     public Dungeon dungeonPrefab;
     public GameObject player;
 
+    public float sceneRotationSpeed;
+
     private Dungeon dungeonInstance;
-    
+
+    private bool isSceneRotating;
+
 	// Use this for initialization
-	void Start ()
+	void Start()
     {
+        isSceneRotating = false;
+
         Setup();
 	}
 	
 	// Update is called once per frame
-	void Update ()
+	void Update()
     {
 		if (Input.GetKeyDown(KeyCode.R))
         {
             Restart();
         }
+
+        if (Input.GetKeyDown(KeyCode.Alpha0) ||
+            Input.GetKeyDown(KeyCode.Keypad0)
+        ) {
+            ToggleSceneRotation();
+        }
+
+        if (isSceneRotating)
+        {
+            dungeonInstance.transform.Rotate(Vector3.up, sceneRotationSpeed * Time.deltaTime);
+        }
 	}
+
+    private void ToggleSceneRotation()
+    {
+        isSceneRotating = !isSceneRotating;
+    }
 
     private void Setup()
     {
