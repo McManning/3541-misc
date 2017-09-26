edc481e4b2742850a4b283e95a1faeabb522e36c
Chase McManning <mcmanning.1@osu.edu>
2017-09-25 21:38:43 -0400

    Add over the shoulder camera experiment

---
+++ Assets/Scripts/FixedCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedCamera : MonoBehaviour
+{
+    public Vector3 offset;
+
+    private Quaternion rotation;
+
+	void Start () {
+        // cache initial rotation
+        rotation = transform.rotation;
+	}
+    
+	void Update () {
+        // force rotation to always be at initial
+        transform.rotation = rotation;
+
+        transform.position = transform.parent.position + offset; // - Vector3.back * 5.0f;
+	}
+}
