89c7734b4f58274ae01bb3087c833818bf9cce4d
Chase McManning <mcmanning.1@osu.edu>
2017-10-26 21:32:50 -0400

    :shark:

---
+++ Assets/Animations/Behaviors/IdleBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleBehavior : StateMachineBehaviour
+{
+    private Shark agent;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        agent = animator.GetComponentInParent<Shark>();
+    }
+    
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        // Aimlessness!
+        agent.transform.position += agent.velocity * Time.deltaTime;
+	}
+}
---
+++ Assets/Animations/Shark.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shark : MonoBehaviour
+{
+    /// <summary>
+    /// Forward FOV in degrees
+    /// </summary>
+    public float fov;
+    
+    // [HideInInspector]
+    public Vector3 velocity;
+
+    // Use this for initialization
+    void Start()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+}
