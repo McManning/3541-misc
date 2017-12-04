72a41a1101971b461cd9828adcb4259ace829c97
Chase McManning <mcmanning.1@osu.edu>
2017-12-03 19:31:22 -0500

    Grow effect and general configuring of the particle engines

---
+++ Assets/Scripts/PortalEffect.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalEffect : MonoBehaviour
+{
+    public float scaleSpeed;
+    public float scaleMax;
+    
+	void Update()
+    {
+        transform.localScale = Vector3.Lerp(
+            transform.localScale,
+            new Vector3(scaleMax, scaleMax, scaleMax), 
+            scaleSpeed * Time.deltaTime
+        );
+    }
+}
